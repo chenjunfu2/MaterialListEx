d20ecca04ce1a0952e8e70d730c94361779892ff
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-04-27 13:44:13 +0800

    测试修改

--- NBT_Test/NBT_Test.cpp
+++ NBT_Test/NBT_Test.cpp
@@ -8,10 +8,30 @@
 #include "..\MaterialListEx\Compression_Utils.hpp"
 
 #include <stdio.h>
+//#include <source_location>
+#include <map>
+
+
+//template <typename T>
+//T func(T)
+//{
+//	auto c = std::source_location::current();
+//	printf("%s %s [%d:%d]\n", c.file_name(), c.function_name(), c.line(), c.column());
+//	return {};
+//}
+
 
 
 int main(int argc, char *argv[])
 {
+	//auto c = std::source_location::current();
+	//
+	//printf("%s %s [%d:%d]\n", c.file_name(), c.function_name(), c.line(), c.column());
+	//
+	//func<int>(1);
+	//func<std::vector<int>>({});
+
+
 	if (argc != 2)
 	{
 		printf("Only one input file is needed\n");
@@ -39,17 +59,53 @@ int main(int argc, char *argv[])
 	}
 
 
+	//以下使用nbt
+	NBT_Node nRoot;
+	if (!NBT_Reader<std::string>::ReadNBT(nRoot, sNbtData))
+	{
+		printf("\nData before the error was encountered:");
+		NBT_Helper::Print(nRoot);
+		return -1;
+	}
+	else
+	{
+		printf("Read Ok!\n");
+	}
 
+	const auto &tmp = nRoot.GetCompound();//获取根下第一个compound，正常情况下根部下只有这一个compound
+	if (tmp.size() != 1)
+	{
+		printf("Error root size");
+		return -1;
+	}
 
+	//输出名称（一般是空字符串）
+	const auto &root = *tmp.begin();
+	printf("root:\"%s\"\n", ANSISTR(U16STR(root.first)).c_str());
 
+	//获取regions，也就是区域，一个投影可能有多个区域（选区）
+	const auto &cpRegions = GetCompound(root.second).GetCompound(MU8STR("Regions"));
+	for (const auto &[RgName, RgCompound] : cpRegions)//遍历选区
+	{
+		//获取方块实体列表
+		const auto &listTileEntity = GetCompound(RgCompound).GetList(MU8STR("TileEntities"));
 
+		//创建map，同名方块实体只保留第一个
+		NBT_Node::NBT_Compound cpdTileEntity{};
 
+		for (const auto &it : listTileEntity)
+		{
+			cpdTileEntity.try_emplace(GetCompound(it).GetString(MU8STR("id")), it);
+		}
 
+		for (const auto &it : cpdTileEntity)
+		{
+			printf("give @s %s\n", it.first.c_str());
+		}
+		//printf("Region:[%s]\n", ANSISTR(U16STR(RgName)).c_str());
+		//NBT_Helper::Print(NBT_Node{ std::move(cpdTileEntity) });
+	}
 
-
-
-
-
-
+	
 	return 0;
 }
